37402d44e2b7eedc12f7cd057c2890c97a010ac7
Durgesh Pratap Singh <durgeshpratap1004@gmail.com>
2026-09-03 23:18:02 +0530

    Time: 6 ms (45.52%) | Memory: 15.9 MB (40.70%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,21 +1,132 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        long long sum = 0;
+    vector<int> findMiniNse(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> Nsemini(n);
+        stack<int> st;
+
+        for(int i=n-1; i>=0; i--)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
+            {
+                st.pop();
+            }
+            Nsemini[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return Nsemini;
+    }
+
+    vector<int> findMiniPse(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> Psemini(n);
+        stack<int> st;
+
         for(int i=0; i<n; i++)
         {
-            int maxi = nums[i];
-            int mini = nums[i];
-            for(int j=i+1; j<n; j++)
+            while(!st.empty() && arr[st.top()]>arr[i])
             {
-                maxi = max(maxi, nums[j]);
-                mini = min(mini, nums[j]);
-                sum = sum + (maxi-mini);
-
+                st.pop();
             }
-           
+            Psemini[i] = st.empty() ? -1 : st.top();
+            st.push(i) ;
         }
-        return sum;
+
+        return Psemini;
+
+    }
+
+    vector<int> findMaxiNse(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> Nsemaxi(n);
+        stack<int> st;
+
+        for(int i=n-1; i>=0; i--)
+        {
+            while(!st.empty() && arr[st.top()]<=arr[i])
+            {
+                st.pop();
+            }
+            Nsemaxi[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return Nsemaxi;
+        
+
+    }
+    vector<int> findMaxiPse(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> Psemaxi(n);
+        stack<int> st;
+
+        for(int i=0; i<n; i++)
+        {
+            while(!st.empty() && arr[st.top()]<arr[i])
+            {
+                st.pop();
+            }
+            Psemaxi[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return Psemaxi;
+
+    }
+
+        
+    long long sumSubarrayMins(vector<int>& arr) {
+        long long total = 0;
+        int n = arr.size();
+
+        vector<int> nse =findMiniNse(arr);
+        vector<int> pse =findMiniPse(arr);
+
+        int mod  = 1000000007;
+
+        for(int i=0; i<n; i++)
+        {
+            int left = i-pse[i];
+            int right = nse[i]-i;
+
+            total = (total +(right*left*1LL*arr[i]));
+        }
+
+        return total;
+    }
+
+        
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        long long total = 0;
+        int n = arr.size();
+
+        vector<int> nse =findMaxiNse(arr);
+        vector<int> pse =findMaxiPse(arr);
+
+        int mod  = 1000000007;
+
+        for(int i=0; i<n; i++)
+        {
+            int left = i-pse[i];
+            int right = nse[i]-i;
+
+            total = (total +(right*left*1LL*arr[i]));
+        }
+
+        return total;
+
+        
+    }
+
+
+    long long subArrayRanges(vector<int>& nums) {
+
+
+        return sumSubarrayMaxs(nums)-sumSubarrayMins(nums);
+
+
+        
     }
 };
