659be9bea2148455b01fa88f2cd458ed9b5fc109
Durgesh Pratap Singh <durgeshpratap1004@gmail.com>
2026-09-15 01:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (76.30%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -23,6 +23,7 @@ public:
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
         vector<int> ds;
         combine(0, candidates, target, ans, ds);
